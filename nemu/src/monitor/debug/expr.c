50c59b627e33c95eb079eff79bc12c158a7e8cd1
tracer-ics2015 <tracer@njuics.org>
2016-03-13 23:11:45 +0800

    >  compile NEMU 141220000 bai Linux bai-ThinkPad-T540p 3.19.0-51-generic #58~14.04.1-Ubuntu SMP Fri Feb 26 22:02:58 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:11:45 up  3:54,  6 users,  load average: 0.26, 0.19, 0.14 f1ba1eeda9619450f2a3563c82efc556ce9746bf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -141,7 +141,7 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-	eval(0 ,nr_token);
+	return eval(0 ,nr_token);
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
 	return 0;
